3c2c05503bc9a89c3f20ddaa896292698e54af25
Ali Martondi <alimartondi88@gmail.com>
2025-10-31 10:55:03 +0700

    feat: Improve meta-them-color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Roboto } from "next/font/google";
 import Header from "@/components/layouts/header";
 import Footer from "@/components/layouts/footer";
 
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { MetaThemeColor } from "@/components/theme/meta-theme-color";
 
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
@@ -19,6 +18,13 @@ export const metadata: Metadata = {
   title: "PLN | Diesel Replacement Program",
   description:
     "Transforming Indonesia's energy landscape through renewable solar PV and battery energy storage systems",
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "hsl(0 0% 100%)" },
+    { media: "(prefers-color-scheme: dark)", color: "hsl(240 10% 4%)" },
+  ],
 };
 
 export default function RootLayout({
@@ -37,7 +43,6 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Header />
-            <MetaThemeColor />
             <main>{children}</main>
             <Footer />
           </ThemeProvider>
